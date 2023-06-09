fe4bbfb78c0d2725983d7b2e8014e6670f879369
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2023-06-09 14:43:15 +0530

    Update cloud-function.js

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -1,9 +1,29 @@
-export default function handler(_, res) {
-  if (process.env.TEST_VARIABLE !== "TEST_VALUE") {
-    throw new Error("Env variable not present");
+const cookieName = 'my-token';
+
+export default function handler(req, res) {
+  console.log('Debug log for deploymentIdentifier: test-deployment-identifier');
+  console.log({cookie: req.headers.cookie});
+  console.log({cookies: req.cookies});
+  console.log(JSON.stringify(req.headers));
+  console.log(JSON.stringify(req));
+
+  if (process.env.TEST_VARIABLE !== 'TEST_VALUE') {
+    throw new Error('Env variable not present');
+  }
+
+  if (!req.headers.cookie.contains(cookieName)) {
+    throw new Error('Cookie not present');
   }
 
   res
     .status(200)
-    .json({ deploymentIdentifier: "cloud-flare" })
+    .setHeader(
+      'set-cookie',
+      'my-cookie=heres-my-cookie; Path=/; HttpOnly; Secure; SameSite=Lax'
+    )
+    .setHeader(
+      'set-cookie',
+      'my-cookie-2=heres-my-cookie-2; Path=/; HttpOnly; Secure; SameSite=Lax'
+    )
+    .json({ deploymentIdentifier: 'DEPLOYMENT_IDENTIFIER' })
 }
